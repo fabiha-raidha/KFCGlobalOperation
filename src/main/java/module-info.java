61e694ce86c03e319fec34546b5245db1673f16a
Your Name <frkhan915@gmail.com>
2025-10-02 09:27:54 +0600

    Initial Commit with LogIn FXML

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module oop.kfcglobaloperation {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens oop.kfcglobaloperation to javafx.fxml;
+    exports oop.kfcglobaloperation;
+}
---
+++ src/main/java/oop/kfcglobaloperation/LogInController.java
@@ -0,0 +1,21 @@
+package oop.kfcglobaloperation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LogInController {
+
+    @FXML
+    private PasswordField passwordTF;
+
+    @FXML
+    private TextField usernameTF;
+
+    @FXML
+    void onLogInButtonClicked(MouseEvent event) {
+
+    }
+
+}
