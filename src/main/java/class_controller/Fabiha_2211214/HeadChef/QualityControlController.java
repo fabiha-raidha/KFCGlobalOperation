3dac392768b32192c6b3782141ec2d593a573d9a
fabiha-raidha <frkhan915@gmail.com>
2025-11-28 00:47:36 +0600

    Fabiha_HeadChefG3QualityControl_setup

---
+++ src/main/java/class_controller/Fabiha_2211214/HeadChef/QualityControlController.java
@@ -0,0 +1,62 @@
+package class_controller.Fabiha_2211214.HeadChef;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class QualityControlController {
+    @javafx.fxml.FXML
+    private Label checklistStatusLabel;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> actionsColumn;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> nextScheduleColumn;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> tempStatusColumn;
+
+    @javafx.fxml.FXML
+    private TableView<?> tempLogTableView;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> tempItemColumn;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> logTypeColumn;
+
+    @javafx.fxml.FXML
+    private TextField tempItemTextField;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> lastCheckColumn;
+
+    @javafx.fxml.FXML
+    private Button viewChecklistButton;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> complianceScoreColumn;
+
+    @javafx.fxml.FXML
+    private TextField tempReadingTextField;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> tempReadingColumn;
+
+    @javafx.fxml.FXML
+    private Label expiryAlertLabel;
+
+    @javafx.fxml.FXML
+    private TableView<?> inspectionLogTableView;
+
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> timestampColumn;
+
+    @javafx.fxml.FXML
+    public void handleSaveNewTemperatureONA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDailyChecklistONA(ActionEvent actionEvent) {
+    }
+}
