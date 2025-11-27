6e004c6bdff96803ffd4b0b5786e92963d6cd7e7
fabiha-raidha <frkhan915@gmail.com>
2025-11-28 01:03:38 +0600

    Fabiha_HeadChefG4AttendanceSheet_setup

---
+++ src/main/java/class_controller/Fabiha_2211214/HeadChef/AttendanceController.java
@@ -0,0 +1,31 @@
+package class_controller.Fabiha_2211214.HeadChef;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class AttendanceController {
+    @javafx.fxml.FXML
+    private TextField lineSearchTextField;
+    @javafx.fxml.FXML
+    private TableView<?> attendanceTableView;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> workerNameColumn;
+    @javafx.fxml.FXML
+    private ComboBox<?> durationComboBox;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> employeeIdColumn;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> statusColumn;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> dateColumn;
+    @javafx.fxml.FXML
+    private TextField employeeSearchTextField;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> workLineColumn;
+
+    @javafx.fxml.FXML
+    public void handleFetchAttendanceONA(ActionEvent actionEvent) {
+    }
+}
