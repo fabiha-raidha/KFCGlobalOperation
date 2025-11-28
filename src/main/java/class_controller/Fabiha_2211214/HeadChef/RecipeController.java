286f91c51b850832e2d855fdbc0559c735181dea
fabiha-raidha <frkhan915@gmail.com>
2025-11-28 22:51:16 +0600

    Fabiha_HeadChefG1RecipeManagement_setup

---
+++ src/main/java/class_controller/Fabiha_2211214/HeadChef/RecipeController.java
@@ -0,0 +1,37 @@
+package class_controller.Fabiha_2211214.HeadChef;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class RecipeController {
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> statusTableColumn;
+    @javafx.fxml.FXML
+    private ComboBox<?> statusFilterComboBox;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> regionsTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> nameTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> priceTableColumn;
+    @javafx.fxml.FXML
+    private TableView<?> recipeTableView;
+    @javafx.fxml.FXML
+    private ComboBox<?> regionFilterComboBox;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> actionsTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<?, ?> dishIdTableColumn;
+    @javafx.fxml.FXML
+    private Label totalRecipeLabel;
+    @javafx.fxml.FXML
+    private TextField dishSearchTextField;
+
+    @javafx.fxml.FXML
+    public void handleFilterRecipesONA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleResetFilterONA(ActionEvent actionEvent) {
+    }
+}
